f27cd5f1741b81bf95d1a84c8694263c0c53c728
mao cong <maocong127@126.com>
2023-04-24 23:52:52 +0800

    m

--- minis/src/com/minis/test/Test1.java
+++ minis/src/com/minis/test/Test1.java
@@ -1,8 +1,11 @@
 package com.minis.test;
 
+import com.minis.beans.NoSuchBeanDefinitionException;
+import com.minis.context.ClassPathXmlApplicationContext;
+
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchBeanDefinitionException {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 	    AService aService=(AService)ctx.getBean("aservice");
 	    aService.sayHello();
